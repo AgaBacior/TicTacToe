b29b1e64f78c3f8da720d53ac4074693fa3d81ef
Agnieszka Bacior <agnieszka.bacior@gmail.com>
2015-06-10 15:02:11 +0200

    Add few comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	
-	// variables  
+	// Variables  
 	var context;
 	var nbCell = 0;
 	var sizeCase = 200;
@@ -15,11 +15,11 @@ $(function(){
 	var imageX;
 	var imageO;	
 
-	// event lauch when form is submited
+	// When form is submitted event are launched
 	$('form').on('submit', function(e){
 		e.preventDefault();
 
-		// This is the caalback we will call when the X and O image will be loade		
+		// This is the callback we will call when the X and O image will be loade		
 		var checkImageLoadShowCanvas = function() {
 			// One image is loaded
 			++nbImageLoaded;
@@ -31,8 +31,7 @@ $(function(){
 		};
 		
 		// We create two image, X and O for drawing on the canvas
-		// When both will be loaded the canvas will be showed
-		
+		// When both will be loaded the canvas will be showed		
 		imageX = document.createElement('img');
 		imageX.src = 'img/x.png';
 		imageX.onload = checkImageLoadShowCanvas;
@@ -41,54 +40,52 @@ $(function(){
 		imageO.src = 'img/o.png';
 		imageO.onload = checkImageLoadShowCanvas;
 		
-		
-		
-		// hidden the form with names
-		// dislay the canvas with the game
+		// Hidden the form with names
 		$('#registeredForm').addClass('hidden');		
-		//$('#game').removeClass('hidden');
 		
-		// take each player's name value 
-
+		// Take each player's name value 
 		setName($('#name1').val(), $('#name2').val());
 
-		// canvas
+		// Take canvas
 		var canvas = document.querySelector('canvas');
 
 		context = canvas.getContext('2d');
 		//context.fillRect(0, 0, 600, 600);
 		
+		// Launch two function initialization and countNbCell
 		initialization();		
 		countNbCell();
 
-		// event tigger each time whether canvas is clicked
+		// Event tigger each time whether canvas is clicked
 		//canvas.addEventListener('click', managePlay);
 		$(context.canvas).on('click', managePlay);
 
-		// if btn quitGame is clicked
+		// When the btn quitGame is clicked quit the game
 		$('#quitGame').on('click', function(){
 			window.location = "index.html";
 		});
 
-		// if btn continue is cliked 
+		// When the btn nextGame is clicked start a new game
 		$('#nextGame').on('click', nextGame);
 
-		// if btn continue is cliked 
+		// When the btn closeBox is clicked hide the alert box 
 		$('#closeBox').on('click', function(){
 			$('#alertBox').addClass('hidden');
 		});
 
 	});
-
+	
+	// Function - take each player's name value 
 	var setName = function (namePlayer1, namePlayer2) {
 
-		//add names and img 'x' or 'o' to DOM
+		// Add names and a tag img 'x' or 'o' to DOM
 		$('div').html('<p>' + namePlayer1 + '<span class="xClass"></span></p><p>' + namePlayer2 + '<span class="oClass"></span></p>');
 
 		name1 = namePlayer1;
 		name2 = namePlayer2;
 	};
 
+	// Count all cells of the array movePlayer
 	var countNbCell = function() {
 		
 		for (var i = 0, l1 = movePlayer.length; i<l1; i++) {
@@ -99,14 +96,20 @@ $(function(){
 			}
 		}
 	};
-	//
+	
+	// Initialize an empty arrays and the variables at the beginning of each game 
 	var initialization = function () {
 		
+		// The empty arrays
 		movePlayer = [['e','e','e'],['e','e','e'],['e','e','e']];
+		
+		// playerX starts the game		
 		playerX = true;
+		
+		// playCount - no one has played
 		playCount = 0;
 
-		// draw lines vertically and horizontally
+		// Canvas - draw lines vertically and horizontally 
 		for(var x = 200.5; x<600; x+=200){
 			context.moveTo(x, 0);
 			context.lineTo(x, 600);			
@@ -117,24 +120,24 @@ $(function(){
 			context.lineTo(600, y);
 		}
 
-		context.strokeStyle ='#6E7171';
+		context.strokeStyle ='#818181';
 		context.stroke();
 	};
 
-	// 
+	// This is the function which manage the game 
 	var managePlay = function(e){
 
-		//get a position of click on the canvas 
+		// Get a position of click on the canvas 
 		var positionX = e.pageX - this.offsetLeft;
 		var positionY = e.pageY - this.offsetTop;
 
-		// convert a position on the canvas on a position a cell in the array 
+		// Convert a position on the canvas on a position a cell in the array 
 		var x = Math.floor(positionX/sizeCase);
 		var y = Math.floor(positionY/sizeCase);
 
 		var caseContent = movePlayer[x][y];
 		
-		// test: if this cell not clicked (item of array ==='e') put 'x' for playerX or 'o' if it's not playerX
+		// Test: if this cell not clicked (item of array ==='e') put 'x' for playerX or 'o' if it's not playerX
 		if (caseContent === 'e') {
 			
 			if (playerX) {
@@ -145,53 +148,55 @@ $(function(){
 			
 			movePlayer[x][y] = caseContent;
 			
-			// add Image on the canvas
+			// Add image on the canvas
 			addImgMove(x, y, playerX);
 
-			// count valable clicks
+			// Count valable clicks
 			playCount++;
 			
-			// after 5 clicks check if someone wins 
+			// After 5 clicks check if someone wins 
 			if (playCount > 4) {
 				
-				// test if someone wins true or false 
+				// Test: if someone wins true or false 
 				var winner = testWinner();				
 				
 				if (winner) {
 					endMsg(true, playerX);
 				} else {
 					
-					// test if empty cells
+					// Test: if there is still empty cell
 					if (playCount === nbCell){
 						endMsg(false);
 					}
 				}				
 			}
 
-			// switch the player who clicks
+			// Switch the player who clicks
 			playerX = !playerX;
 
 		} else {
+
+			// If this cell is clicked dispay alertBox
 			$('#alertBox').removeClass('hidden');
 		}
 	};
 
-	// add an image of 'x' or 'o ' depending on the player 
+	// Add an image of 'x' or 'o ' depending on the player 
 	var addImgMove = function(posX, posY, playerX){
 		
 		// calculete a position on the canvas, deponding on the size of image
 		var posCaseX = (posX*sizeCase)+80;
 		var posCaseY = (posY*sizeCase)+80;
 		
+		// Add an image 'x' or 'o '
 		if (playerX){
-			// add an image 
 			context.drawImage(imageX, posCaseX, posCaseY);
 		} else {
 			context.drawImage(imageO, posCaseX, posCaseY);
 		}
 	};
 	
-	//
+	// Test if someone won. Compare the contents of array cells
 	var testWinner = function() {
 			
 		if (movePlayer[0][0] === movePlayer[1][1] && movePlayer[1][1] === movePlayer[2][2]){
@@ -226,30 +231,39 @@ $(function(){
 		
 		return false;
 	};
+
+	// If someone won
 	var endMsg = function (winner) {
 		
+		// Dislay the message depending on the result
 		if (winner) {
-			$('#boxGameOver div').html('<p>Wygral'+ (playerX ? ' krzyzyk' : 'o kolko') + '</p>');
+			$('#boxGameOver div').html('<p>Wygrał gracz: '+ (playerX ? name1 : name2) + '</p>');
 		} else {
-			$('#boxGameOver div').html('<p>Nikt nie wygral' + '</p>');			
+			$('#boxGameOver div').html('<p>Nikt nie wygrał</p>');			
 		}
 
 		$('#boxGameOver').removeClass('hidden');
 
-		//remove click on canvas
+		// Remove click on canvas
 		$(context.canvas).off('click',managePlay);
 	};
 
+	// Start a new game
 	var nextGame = function() {
 
+		// Hide the box Game Over
 		$('#boxGameOver').addClass('hidden');
-		// initialization variable
 		
+		// Clear the canvas
 		context.clearRect(0, 0, 600, 600);
-		
-		setName(name2, name1);
-		initialization();
 
+		// Switch players		
+		setName(name2, name1);
+
+		// Initialize the array and variables				
+		initialization();
+		
+		// Start a new game				
 		$(context.canvas).on('click', managePlay);
 
 	};
